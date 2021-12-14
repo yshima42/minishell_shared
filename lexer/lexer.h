04b1b547ff0db4fe727e8d72f5e30700eebdcfdd
Hiroki Yoshie <hyoshie@c1r22s8.42tokyo.jp>
2021-12-14 10:54:08 +0900

    fix:split header
    test long commit message

--- lexer/lexer.h
+++ lexer/lexer.h
@@ -6,7 +6,7 @@
 /*   By: hyoshie <hyoshie@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 15:02:31 by hyoshie           #+#    #+#             */
-/*   Updated: 2021/12/14 10:28:15 by hyoshie          ###   ########.fr       */
+/*   Updated: 2021/12/14 10:53:32 by hyoshie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,11 @@
 # include  <stdio.h>
 # include  <stdbool.h>
 # include "../libft/mylibft.h"
+# include "token.h"
+
 # include "color.h"
 
 # define EQUAL 0
-
-//type is better?
-enum e_kind
-{
-	WORD,
-	HEREDOC,
-	APPEND,
-	IN_REDIRECT,
-	OUT_REDIRECT,
-	PIPE,
-	LAST,
-};
-
-enum e_quote
-{
-	NONE,
-	SINGLE,
-	DOUBLE,
-};
-
-//when token to node, nyokota use funcptr
-typedef struct s_token
-{
-	struct s_token	*prev;
-	struct s_token	*next;
-	char			*word;
-	enum e_kind		kind;
-	enum e_quote	quote;
-}	t_token;
 
 t_token	*to_tokenlist(char *line);
 size_t	tkn_strlen(const char *start);
@@ -58,10 +31,5 @@ bool	is_operator(char c);
 bool	is_2chr_operator(const char *str);
 bool	is_quote(char c);
 bool	is_end(char c);
-t_token	*tkn_lstnew(char *str);
-t_token	*tkn_lstlast(t_token *lst);
-void	tkn_lstadd_back(t_token **lst, t_token *new);
-void	tkn_lstdelone(t_token *lst, void (*del)(void *));
-void	tkn_lstclear(t_token	**lst, void(*del)(void *));
 
 #endif /* LEXER_H */
