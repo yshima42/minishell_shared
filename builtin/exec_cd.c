de0c5a8bb92940252242f8b16b15f37c3d929e27
yshimazu <shimazuyuki7@gmail.com>
2021-12-18 08:23:01 +0900

    cd_fix_start

--- builtin/exec_cd.c
+++ builtin/exec_cd.c
@@ -6,7 +6,7 @@
 /*   By: yshimazu <yshimazu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 14:22:20 by yshimazu          #+#    #+#             */
-/*   Updated: 2021/12/17 14:39:14 by yshimazu         ###   ########.fr       */
+/*   Updated: 2021/12/18 08:18:35 by yshimazu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 int	exec_cd(char **args, t_info *info)
 {
-	if (args[1] == NULL)
+	if (args[1] == NULL) //ディレクトリオペランド指定なし
 	{
 		if (!mini_getenv("HOME", info))
+		{
+			ft_putstr_fd("minishell: cd: HOME not set\n", 2);
 			return (CONTINUE);//add error handring
+		}
 		if (chdir(mini_getenv("HOME", info)) != 0)
 			perror("cd");
 	}
