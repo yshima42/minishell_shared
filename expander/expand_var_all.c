9a82eb795b3898d9f8e24fc6afaa463970b85f60
Hiroki Yoshie <hyoshie@c1r22s8.42tokyo.jp>
2021-12-19 15:08:26 +0900

    fix:$HOMEa<CR>

--- expander/expand_var_all.c
+++ expander/expand_var_all.c
@@ -6,7 +6,7 @@
 /*   By: hyoshie <hyoshie@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 22:54:15 by hyoshie           #+#    #+#             */
-/*   Updated: 2021/12/16 16:35:22 by hyoshie          ###   ########.fr       */
+/*   Updated: 2021/12/19 15:07:04 by hyoshie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,6 @@ t_token	*expand_var_all(t_token *tokens, t_dict *env)
 		tokens = tokens->next;
 	}
 	head = remove_empty_token(head);
+	g_exit_status = 0;
 	return (head);
 }
