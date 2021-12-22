91b05bcf471454790be2e7b50cbdb95cf28f1ca9
Hiroki Yoshie <dymn9.term@gmail.com>
2021-12-23 01:27:15 +0900

    fix:export_no_arg

--- builtin/utils.c
+++ builtin/utils.c
@@ -6,7 +6,7 @@
 /*   By: hyoshie <hyoshie@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 10:15:03 by hyoshie           #+#    #+#             */
-/*   Updated: 2021/12/19 17:37:23 by hyoshie          ###   ########.fr       */
+/*   Updated: 2021/12/23 01:24:49 by hyoshie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,13 @@ bool	has_one_arg(char **cmd)
 		return (false);
 }
 
+static void	ft_putstr_fd_dquate(char *str, int fd)
+{
+	ft_putchar_fd('\"', fd);
+	ft_putstr_fd(str, fd);
+	ft_putchar_fd('\"', fd);
+}
+
 void	show_environment(t_dict *env, enum e_cmd cmd)
 {
 	t_dict	*t_env;
@@ -48,9 +55,13 @@ void	show_environment(t_dict *env, enum e_cmd cmd)
 		ft_putstr_fd(t_env->key, STDOUT_FILENO);
 		ft_putstr_fd("=", STDOUT_FILENO);
 		if (t_env->value != NULL)
-			ft_putendl_fd(t_env->value, STDOUT_FILENO);
-		else
-			ft_putchar_fd('\n', STDOUT_FILENO);
+		{
+			if (cmd == EXPORT)
+				ft_putstr_fd_dquate(t_env->value, STDOUT_FILENO);
+			else
+				ft_putstr_fd(t_env->value, STDOUT_FILENO);
+		}
+		ft_putchar_fd('\n', STDOUT_FILENO);
 		t_env = t_env->next;
 	}	
 }
